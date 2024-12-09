1c871ba6bcdcb80b521c9311ad4cd7eead7ede20
felipe.r.moran@gmail.com <felipe.r.moran@gmail.com>
2024-12-09 20:29:10 -0300

    queriesUpdt

--- src/queries.ts
+++ src/queries.ts
@@ -1,10 +1,10 @@
 const queries = {
     getUsers: "SELECT * FROM user",
-    getUserById: "SELECT * FROM user WHERE id = $1",
-    checkUser: "SELECT * FROM user WHERE name = $1", 
-    addUser: "INSERT INTO user (name, number, email, CPF, status) VALUES ($1, $2, $3, $4, $5)",
-    deleteUser: "DELETE FROM user WHERE id = $1",
-    updateUser: "UPDATE user SET name = $1, number = $2, email = $3, CPF = $4, status = $5 WHERE id = $6"
+    getUserById: "SELECT * FROM user WHERE user_id = $1",
+    checkUser: "SELECT * FROM user WHERE username = $1", 
+    addUser: "INSERT INTO user (username, phone_nr, email, cpf, user_st) VALUES ($1, $2, $3, $4, $5)",
+    deleteUser: "DELETE FROM user WHERE user_id = $1",
+    updateUser: "UPDATE user SET username = $1, phone_nr = $2, email = $3, cpf = $4, user_st = $5 WHERE user_id = $6"
 };
 
 export default queries;
