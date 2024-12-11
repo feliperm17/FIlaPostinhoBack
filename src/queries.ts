26da44b719e040d47f53e3575cedb46175684c6d
Andre F Baretta <andrefbaretta@gmail.com>
2024-12-11 04:12:15 -0300

    Fixed wrong table name

--- src/queries.ts
+++ src/queries.ts
@@ -1,10 +1,10 @@
 const queries = {
-    getUsers: "SELECT * FROM user",
-    getUserById: "SELECT * FROM user WHERE user_id = $1",
-    checkUser: "SELECT * FROM user WHERE username = $1", 
-    addUser: "INSERT INTO user (username, phone_nr, email, cpf, user_st) VALUES ($1, $2, $3, $4, $5)",
-    deleteUser: "DELETE FROM user WHERE user_id = $1",
-    updateUser: "UPDATE user SET username = $1, phone_nr = $2, email = $3, cpf = $4, user_st = $5 WHERE user_id = $6"
+    getUsers: "SELECT * FROM User",
+    getUserById: "SELECT * FROM User WHERE user_id = $1",
+    checkUser: "SELECT * FROM User WHERE username = $1", 
+    addUser: "INSERT INTO User (username, phone_nr, email, cpf, user_st) VALUES ($1, $2, $3, $4, $5)",
+    deleteUser: "DELETE FROM User WHERE user_id = $1",
+    updateUser: "UPDATE User SET username = $1, phone_nr = $2, email = $3, cpf = $4, user_st = $5 WHERE user_id = $6"
 };
 
 export default queries;
