1b18250d5ff33612b7ab5e2cf6691e081d9800a7
Andre F Baretta <andrefbaretta@gmail.com>
2024-12-15 17:27:53 -0300

    Script fixes

--- src/scripts/create_db.sql
+++ src/scripts/create_db.sql
@@ -1,57 +1,50 @@
-DROP TABLE IF EXISTS Admin;
-DROP TABLE IF EXISTS Address;
-DROP TABLE IF EXISTS QueueItem;
-DROP TABLE IF EXISTS Queue;
-DROP TABLE IF EXISTS Specialty;
-DROP TABLE IF EXISTS User;
-
-
-CREATE TABLE User(
-   user_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Account(
+   account_id SERIAL PRIMARY KEY,
    username VARCHAR(50) NOT NULL,
    phone_nr CHAR(13) NOT NULL,
    email VARCHAR(50) NOT NULL,
    cpf CHAR(13) NOT NULL,
-   user_st NUMERIC NOT NULL,
+   account_st NUMERIC NOT NULL,
+   password_hash VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE Specialty(
+CREATE TABLE IF NOT EXISTS Specialty(
    specialty_id SERIAL PRIMARY KEY,
-   specialty_name VARCHAR(50),
+   specialty_name VARCHAR(50)
 );
 
-CREATE TABLE Queue(
+CREATE TABLE IF NOT EXISTS Queue(
    queue_id SERIAL PRIMARY KEY,
-   specialty NUMERIC,
+   specialty INTEGER,
    queue_dt DATE NOT NULL,
    position_nr NUMERIC NOT NULL,
    queue_size NUMERIC NOT NULL,
-   FOREIGN KEY(specialty) REFERENCES Specialty(specialty_id),
+   FOREIGN KEY(specialty) REFERENCES Specialty(specialty_id)
 );
 
-CREATE TABLE QueueItem(
-   queue_id NUMERIC,
-   user_id NUMERIC,
+CREATE TABLE IF NOT EXISTS QueueItem(
+   queue_id INTEGER,
+   account_id INTEGER,
    item_pr NUMERIC NOT NULL,
    position NUMERIC NOT NULL,
    item_st NUMERIC NOT NULL,
    FOREIGN KEY(queue_id) REFERENCES Queue(queue_id),
-   FOREIGN KEY(user_id) REFERENCES User(user_id),
-   PRIMARY KEY(queue_id, user_id),
-)
+   FOREIGN KEY(account_id) REFERENCES Account(account_id),
+   PRIMARY KEY(queue_id, account_id)
+);
 
-CREATE TABLE Address(
-   user_id NUMERIC,
+CREATE TABLE IF NOT EXISTS Address(
+   account_id INTEGER,
    address_id SERIAL,
    street CHAR(50) NOT NULL,
    city CHAR(50) NOT NULL,
    complement CHAR(50),
    postal_code VARCHAR(10),
-   FOREIGN KEY(user_id) REFERENCES User(user_id),
-   PRIMARY KEY(user_id, address_id),
-)
+   FOREIGN KEY(account_id) REFERENCES Account(account_id),
+   PRIMARY KEY(account_id, address_id)
+);
 
-CREATE TABLE Admin(
-   user_id NUMERIC PRIMARY KEY,
-   FOREIGN KEY(user_id) REFERENCES User(user_id),
-)
+CREATE TABLE IF NOT EXISTS Admin(
+   account_id INTEGER PRIMARY KEY,
+   FOREIGN KEY(account_id) REFERENCES Account(account_id)
+);
