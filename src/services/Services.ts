46159fe587b5ade7dd22884332aac6b3370d58be
Andre F Baretta <andrefbaretta@gmail.com>
2024-12-18 16:59:08 -0300

    Added queueService object

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,5 +1,7 @@
 import { UserService } from './UserService';
 import { SpecialtyService } from './SpecialtyService';
+import { QueueService } from './QueueService';
 
+export const queueService = new QueueService();
 export const userService = new UserService();
 export const specialtyService = new SpecialtyService();
