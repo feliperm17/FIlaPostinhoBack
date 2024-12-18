eeae8345c2a1c1e3c662e2a4612c99ae5669b578
Andre F Baretta <andrefbaretta@gmail.com>
2024-12-18 16:52:50 -0300

    Added authentication and permission checking to the queue routes

--- src/routes/queueRoutes.ts
+++ src/routes/queueRoutes.ts
@@ -1,16 +1,26 @@
 import express, { Request, Response } from 'express';
-import controller from '../controllers/controller';
+import { queueController } from '../controllers/controller';
+import { authentication } from '../middlewares/authentication';
+import { checkPermissions } from '../middlewares/permission';
 
 const queueRoutes = express.Router();
-const queueController = new controller.QueueController();
 
-queueRoutes.post('/queue', (req: Request, res: Response) => {
+queueRoutes.post('/queue',
+    authentication,
+    checkPermissions(true), 
+    (req: Request, res: Response) => {
     queueController.createQueue(req, res);
 });
-queueRoutes.get('/queue', (req: Request, res: Response) => {
+queueRoutes.get('/queue',
+    authentication,
+    checkPermissions(true), 
+    (req: Request, res: Response) => {
     queueController.findAllQueues(req, res)}
 );
-queueRoutes.get('/queue/:id', (req: Request, res: Response) => {
+queueRoutes.get('/queue/:id',
+    authentication,
+    checkPermissions(true), 
+    (req: Request, res: Response) => {
     queueController.findQueueById(req, res)
 });
 
