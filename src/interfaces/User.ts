8a84ace677d4c7a2f6faa32c55d045ca00b22a74
Andre F Baretta <andrefbaretta@gmail.com>
2024-12-18 00:34:35 -0300

    User and UserJWT Interfaces

---
+++ src/interfaces/User.ts
@@ -0,0 +1,14 @@
+
+export interface UserInterface {
+  id?: number;
+  username: string; 
+  phone_nr: string; 
+  email: string; 
+  cpf: string; 
+  account_st: number;
+  password: string;
+}
+
+export interface UserJwtInterface {
+  email: string;
+}
