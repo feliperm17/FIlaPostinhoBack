6870f6271ac59f91e01f268b37a19040aed6a891
Andre F Baretta <andrefbaretta@gmail.com>
2024-12-18 00:39:54 -0300

    jwt generation and verification functions

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
-import fs from 'fs';
-import path from 'path';
+
 dotenv.config();
 
 const pool = new Pool({
@@ -11,16 +10,5 @@ const pool = new Pool({
     database: process.env.PGDATABASE,
     port: parseInt(process.env.PGPORT),
 });
-
-const runScript = async (filepath: string) => {
-    const script = fs.readFileSync(path.join(__dirname,filepath)).toString();
-    try{
-        await pool.query(script);
-    }catch(error){
-        console.error("Erro ao criar o Banco de Dados: ", error.message);
-    }
-};
-
-runScript("./scripts/create_db.sql");
 
 export default pool;
