008f98077b35714875973202be8282976669ab22
Andre F Baretta <andrefbaretta@gmail.com>
2025-02-23 17:44:33 -0300

    User permission changes, user admin status changed from account_st to is_admin

--- src/scripts/create_db.sql
+++ src/scripts/create_db.sql
@@ -2,15 +2,18 @@ CREATE TABLE IF NOT EXISTS Account(
    account_id SERIAL PRIMARY KEY,
    username VARCHAR(50) NOT NULL,
    phone_nr VARCHAR(15) NOT NULL,
-   email VARCHAR(50) NOT NULL,
-   cpf CHAR(11) NOT NULL,
+   email VARCHAR(50) UNIQUE NOT NULL,
+   cpf CHAR(11) UNIQUE NOT NULL,
    account_st NUMERIC NOT NULL,
+   is_admin BOOLEAN NOT NULL DEFAULT false,
    password_hash VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Specialty(
    specialty_id SERIAL PRIMARY KEY,
-   specialty_name VARCHAR(50)
+   specialty_name VARCHAR(50),
+   available_days SMALLINT DEFAULT 0,
+   estimated_time SMALLINT
 );
 
 CREATE TABLE IF NOT EXISTS Queue(
@@ -26,25 +29,9 @@ CREATE TABLE IF NOT EXISTS QueueItem(
    queue_id INTEGER,
    account_id INTEGER,
    item_pr NUMERIC NOT NULL,
-   position NUMERIC NOT NULL,
-   item_st NUMERIC NOT NULL,
+   entry_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   item_st NUMERIC NOT NULL, 
    FOREIGN KEY(queue_id) REFERENCES Queue(queue_id),
    FOREIGN KEY(account_id) REFERENCES Account(account_id),
    PRIMARY KEY(queue_id, account_id)
-);
-
-CREATE TABLE IF NOT EXISTS Address(
-   account_id INTEGER,
-   address_id SERIAL,
-   street CHAR(50) NOT NULL,
-   city CHAR(50) NOT NULL,
-   complement CHAR(50),
-   postal_code VARCHAR(10),
-   FOREIGN KEY(account_id) REFERENCES Account(account_id),
-   PRIMARY KEY(account_id, address_id)
-);
-
-CREATE TABLE IF NOT EXISTS Admin(
-   account_id INTEGER PRIMARY KEY,
-   FOREIGN KEY(account_id) REFERENCES Account(account_id)
 );
