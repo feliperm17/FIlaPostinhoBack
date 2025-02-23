44e1cebc1a4f60e5f0a2166fed552bb7a1c99f1e
Andre F Baretta <andrefbaretta@gmail.com>
2025-02-23 17:45:35 -0300

    added days of the week to specialties

--- src/queries/specialtyQueries.ts
+++ src/queries/specialtyQueries.ts
@@ -1,8 +1,8 @@
 const specialtyQueries = {
     getSpecialties: "SELECT * FROM Specialty",
     getSpecialtyById: "SELECT * FROM Specialty WHERE specialty_id = $1",
-    addSpecialty: "INSERT INTO Specialty (specialty_name) VALUES ($1) RETURNING *",
-    updateSpecialty: "UPDATE Specialty SET specialty_name = $1 WHERE specialty_id = $2 RETURNING *",
+    addSpecialty: "INSERT INTO Specialty (specialty_name, available_days, estimated_time) VALUES ($1, $2, $3) RETURNING *",
+    updateSpecialty: "UPDATE Specialty SET specialty_name = $1, available_days = $2, estimated_time = $3 WHERE specialty_id = $4 RETURNING *",
     deleteSpecialty: "DELETE FROM Specialty WHERE specialty_id = $1",
     checkSpecialtyName: "SELECT * FROM Specialty WHERE specialty_name = $1"
 };
